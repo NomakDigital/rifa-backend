3d03d45be64f6633b8b174551e48c65ab7a91050
Paulo Ricardo - Nomak <nomakdigital@gmail.com>
2026-04-03 02:54:37 -0300

    Update pagamento.js

--- routes/pagamento.js
+++ routes/pagamento.js
@@ -8,42 +8,45 @@ mercadopago.configure({
 
 const Pedido = require("../models/Pedido");
 
+// ROTA PIX
 router.post("/criar", async (req, res) => {
-  const { nome, campanhaId, qtd } = req.body;
+  try {
+    const { nome, campanhaId, qtd } = req.body;
 
-  const numeros = [];
-  for (let i = 0; i < qtd; i++) {
-    numeros.push(Math.floor(Math.random() * 10000));
+    const numeros = [];
+    for (let i = 0; i < qtd; i++) {
+      numeros.push(Math.floor(Math.random() * 10000));
+    }
+
+    const valor = qtd * 1;
+
+    const pedido = await Pedido.create({
+      nome,
+      campanhaId,
+      numeros,
+      valor
+    });
+
+    const pagamento = await mercadopago.payment.create({
+      transaction_amount: valor,
+      description: "Compra de números",
+      payment_method_id: "pix",
+      payer: { email: "teste@test.com" }
+    });
+
+    res.json({
+      pix: pagamento.body.point_of_interaction.transaction_data.qr_code,
+      copiaCola: pagamento.body.point_of_interaction.transaction_data.qr_code_base64,
+      pedidoId: pedido._id
+    });
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ erro: "Erro no pagamento" });
   }
-
-  const valor = qtd * 1;
-
-  const pedido = await Pedido.create({
-    nome,
-    campanhaId,
-    numeros,
-    valor
-  });
-
-  const pagamento = await mercadopago.payment.create({
-    transaction_amount: valor,
-    description: "Compra de números",
-    payment_method_id: "pix",
-    payer: { email: "teste@test.com" }
-  });
-
-  res.json({
-    pix: pagamento.body.point_of_interaction.transaction_data.qr_code,
-    copiaCola: pagamento.body.point_of_interaction.transaction_data.qr_code_base64,
-    pedidoId: pedido._id
-  });
 });
 
-module.exports = router;
-
-const express = require("express");
-const router = express.Router();
-
+// ROTA TESTE
 router.get("/", (req, res) => {
   res.json({ msg: "rota pagamento ok" });
 });
