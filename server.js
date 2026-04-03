a5ac7239081df443bdb51c4f25c51728565e704b
Paulo Ricardo - Nomak <nomakdigital@gmail.com>
2026-04-03 02:11:30 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,60 +1,19 @@
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-let campanha = {
-  total: 100000,
-  vendidos: []
-};
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Mongo conectado"))
+  .catch(err => console.log(err));
 
-// gerar números
-function gerarNumeros(qtd) {
-  let nums = [];
+app.use("/campanha", require("./routes/campanha"));
+app.use("/pagamento", require("./routes/pagamento"));
 
-  while (nums.length < qtd) {
-    let n = Math.floor(Math.random() * campanha.total) + 1;
-
-    if (!campanha.vendidos.includes(n) && !nums.includes(n)) {
-      nums.push(n);
-    }
-  }
-
-  return nums;
-}
-
-// comprar
-app.post('/comprar', (req, res) => {
-  const { qtd, nome } = req.body;
-
-  const numeros = gerarNumeros(qtd);
-
-  campanha.vendidos.push(...numeros);
-
-  res.json({
-    sucesso: true,
-    numeros
-  });
-});
-
-// status
-app.get('/status', (req, res) => {
-  res.json({
-    total: campanha.total,
-    vendidos: campanha.vendidos.length
-  });
-});
-
-// sortear
-app.get('/sortear', (req, res) => {
-  const vencedor = campanha.vendidos[
-    Math.floor(Math.random() * campanha.vendidos.length)
-  ];
-
-  res.json({ vencedor });
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('Servidor rodando'));
+app.listen(3000, () => console.log("Servidor rodando"));
