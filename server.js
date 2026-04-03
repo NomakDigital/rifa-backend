11a269f842514a21c4a4eba27c00aea884e39994
Paulo Ricardo - Nomak <nomakdigital@gmail.com>
2026-04-03 02:32:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,18 +9,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// conectar mongo
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("Mongo conectado"))
   .catch(err => console.log(err));
 
+// ROTAS
 app.use("/campanha", require("./routes/campanha"));
 app.use("/pagamento", require("./routes/pagamento"));
 
-app.listen(3000, () => console.log("Servidor rodando"));
-
-app.use("/campanha", require("./routes/campanha"));
-app.use("/pagamento", require("./routes/pagamento"));
-
+// ROTA PRINCIPAL (IMPORTANTE)
 app.get("/", (req, res) => {
   res.send("API Rifa rodando 🚀");
 });
+
+// INICIAR SERVIDOR (SEMPRE POR ÚLTIMO)
+app.listen(3000, () => console.log("Servidor rodando"));
