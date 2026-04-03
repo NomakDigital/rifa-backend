2bcbc966ebc04715025123816c10bf8efac869b3
Paulo Ricardo - Nomak <nomakdigital@gmail.com>
2026-04-03 02:01:38 -0300

    Update Pedido.js

--- models/Pedido.js
+++ models/Pedido.js
@@ -1,1 +1,12 @@
 
+const mongoose = require("mongoose");
+
+const Pedido = mongoose.model("Pedido", {
+  campanhaId: String,
+  nome: String,
+  numeros: Array,
+  valor: Number,
+  pago: { type: Boolean, default: false }
+});
+
+module.exports = Pedido;
