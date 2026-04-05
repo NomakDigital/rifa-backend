1331570fc03e839f2bec15c68280e5e307d0226a
Paulo Ricardo - Nomak <nomakdigital@gmail.com>
2026-04-05 00:19:10 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,31 +4,30 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+const expirarPedidos = require("./jobs/expirarPedidos");
+
 const app = express();
 
-// middlewares
 app.use(cors());
 app.use(express.json());
 
-// conexão com MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("🔥 Mongo conectado"))
-  .catch(err => console.log("❌ Erro Mongo:", err));
+  .catch(err => console.log(err));
 
-// rotas
 app.use("/campanha", require("./routes/campanha"));
 app.use("/pagamento", require("./routes/pagamento"));
-app.use("/webhook", require("./routes/webhook")); // 👈 webhook aqui
+app.use("/webhook", require("./routes/webhook"));
 
-// rota raiz
 app.get("/", (req, res) => {
   res.send("API rodando 🚀");
 });
 
-// porta dinâmica (IMPORTANTE pro Render)
+// roda expiração a cada 1 minuto
+setInterval(expirarPedidos, 60000);
+
 const PORT = process.env.PORT || 3000;
 
-// iniciar servidor
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando na porta ${PORT}`);
+  console.log("🚀 Servidor rodando");
 });
