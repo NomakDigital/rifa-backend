424431871c76148e8592e5613f1e63c7e5663c99
Paulo Ricardo - Nomak <nomakdigital@gmail.com>
2026-04-04 23:11:37 -0300

    Update server.js
    webhook ADD

--- server.js
+++ server.js
@@ -6,29 +6,29 @@ const cors = require("cors");
 
 const app = express();
 
+// middlewares
 app.use(cors());
 app.use(express.json());
+
+// conexão com MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("🔥 Mongo conectado"))
+  .catch(err => console.log("❌ Erro Mongo:", err));
+
+// rotas
+app.use("/campanha", require("./routes/campanha"));
+app.use("/pagamento", require("./routes/pagamento"));
+app.use("/webhook", require("./routes/webhook")); // 👈 webhook aqui
 
 // rota raiz
 app.get("/", (req, res) => {
   res.send("API rodando 🚀");
 });
 
-// rotas
-app.use("/campanha", require("./routes/campanha"));
-app.use("/pagamento", require("./routes/pagamento"));
+// porta dinâmica (IMPORTANTE pro Render)
+const PORT = process.env.PORT || 3000;
 
-// 🔥 conexão + start
-mongoose.connect("mongodb+srv://nomakdigital_db_user:nfKaQz2Srnl26B8L@rifadb.1r24pla.mongodb.net/rifa?retryWrites=true&w=majority", {
-  serverSelectionTimeoutMS: 5000
-})
-.then(() => {
-  console.log("🔥 Mongo conectado DE VERDADE");
-
-  app.listen(3000, () => {
-    console.log("🚀 Servidor rodando");
-  });
-})
-.catch(err => {
-  console.log("❌ ERRO REAL DO MONGO:", err);
+// iniciar servidor
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
