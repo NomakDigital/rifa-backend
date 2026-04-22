d435412b40d616bf676208bd1c27ccd3e6ad49f1
Paulo Ricardo - Nomak <nomakdigital@gmail.com>
2026-04-21 23:44:16 -0300

    Update Pedido.js

--- models/Pedido.js
+++ models/Pedido.js
@@ -5,16 +5,11 @@ const PedidoSchema = new mongoose.Schema({
   campanhaId: String,
   numeros: [Number],
   valor: Number,
-  pagamentoId: String,
+
   status: {
     type: String,
     default: "pendente"
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
   }
 });
 
-// 🔥 CORREÇÃO AQUI
 module.exports = mongoose.models.Pedido || mongoose.model("Pedido", PedidoSchema);
