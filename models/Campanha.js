a0f15690e36e41a72fadf2d552c2070e10e6929c
Paulo Ricardo - Nomak <nomakdigital@gmail.com>
2026-04-22 00:01:25 -0300

    Update Campanha.js

--- models/Campanha.js
+++ models/Campanha.js
@@ -2,15 +2,35 @@ const mongoose = require("mongoose");
 
 const CampanhaSchema = new mongoose.Schema({
   nome: String,
-  preco: Number,
+
+  // preço padrão por número
+  preco: {
+    type: Number,
+    required: true
+  },
+
+  // PROMOÇÃO (opcional)
+  promocaoQtd: {
+    type: Number,
+    default: 0
+  },
+
+  promocaoPreco: {
+    type: Number,
+    default: 0
+  },
+
   numerosTotal: {
     type: Number,
     default: 10000
   },
+
   numerosVendidos: {
     type: [Number],
     default: []
   }
 });
 
-module.exports = mongoose.models.Campanha || mongoose.model("Campanha", CampanhaSchema);
+module.exports =
+  mongoose.models.Campanha ||
+  mongoose.model("Campanha", CampanhaSchema);
